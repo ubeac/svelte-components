38344cb1aaa59055d3178581aa9ed840016d0b21
TheHadiAhmadi <TheHadiAhmadi@gmail.com>
2021-12-11 19:19:40 +0430

    add some example files

---
+++ src/Alert.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    let a: number = 0
+
+    a = a + 5
+
+</script>
+<div class="bg-gray-700 text-white p-2">
+    Alert Component {a}
+</div>
---
+++ src/index.js
@@ -0,0 +1,7 @@
+export function log(...message) {
+    console.log(message)
+}
+
+export {add, sub} from './functions'
+
+export {default as Alert} from './Alert.svelte'

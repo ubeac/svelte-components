7d741d97d9e3c5cf1b2dbeec7fec796cf76faf76
TheHadiAhmadi <TheHadiAhmadi@gmail.com>
2021-12-12 16:37:28 +0430

    add breadcrump components

--- example/src/App.svelte
+++ example/src/App.svelte
@@ -1,9 +1,16 @@
 <script>
      import Alert from './components/Alert.svelte'
      import Card from './components/Card.svelte'
+     import {Breadcrumb, BreadcrumbItem} from '@ubeac/svelte-components'
 
 </script>
 
 <Alert />
 <Card />
+<h1 class="text-lg m-2">Breadcrumb</h1>
+<Breadcrumb>
+     <BreadcrumbItem>Hello</BreadcrumbItem>
+     <BreadcrumbItem>World</BreadcrumbItem>
+     <BreadcrumbItem active>123</BreadcrumbItem>
+</Breadcrumb>
 <div class="p-4 bg-gray-200 text-red-600">Svelte App</div>
--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
 export {default as Alert} from './alert/Alert.svelte'
 export {default as Card} from './Card.svelte'
+export {default as Breadcrumb} from './breadcrumb/Breadcrumb.svelte'
+export {default as BreadcrumbItem} from './breadcrumb/BreadcrumbItem.svelte'

8a72918c62748a59fdc22c79eb0aa61c8d987a00
TheHadiAhmadi <TheHadiAhmadi@gmail.com>
2021-12-12 20:20:03 +0430

    add carousel component and example

--- example/src/App.svelte
+++ example/src/App.svelte
@@ -4,6 +4,7 @@
      import {Avatar, AvatarGroup} from '@ubeac/svelte-components'
      import { Badge } from '@ubeac/svelte-components'
      import {Breadcrumb, BreadcrumbItem} from '@ubeac/svelte-components'
+     import {Carousel, CarouselItem} from '@ubeac/svelte-components'
 
 </script>
 
@@ -32,6 +33,17 @@
      <BreadcrumbItem>World</BreadcrumbItem>
      <BreadcrumbItem active>123</BreadcrumbItem>
 </Breadcrumb>
+
+
+<h1 class="text-lg m-2">Carousel</h1>
+<Carousel class="max-w-md" center>
+     {#each Array.from({length: 7}).fill(0) as item, index}
+          <CarouselItem class="m-1 rounded-box shadow overflow-hidden">
+               <img class="max-w-sm" alt="carousel {index}" src="/example/images/small/img-{index+1}.jpg">
+          </CarouselItem>
+     {/each}
+
+</Carousel>
 
 
 <div class="p-4 bg-gray-200 text-red-600">Svelte App</div>

d6811b25eac2dabc5210b7c22a616a141223e145
TheHadiAhmadi <TheHadiAhmadi@gmail.com>
2021-12-12 16:32:28 +0430

    add badge component

--- example/src/App.svelte
+++ example/src/App.svelte
@@ -1,9 +1,14 @@
 <script>
      import Alert from './components/Alert.svelte'
      import Card from './components/Card.svelte'
+     import { Badge } from '@ubeac/svelte-components'
 
 </script>
 
 <Alert />
 <Card />
+<h1 class="text-lg m-2">Badge</h1>
+<Badge variant="secondary">New</Badge>
+<Badge>prima ry</Badge>
+<Badge size="lg">lg</Badge>
 <div class="p-4 bg-gray-200 text-red-600">Svelte App</div>
--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
 export {default as Alert} from './alert/Alert.svelte'
+export {default as Badge} from './badge/Badge.svelte'
 export {default as Card} from './Card.svelte'

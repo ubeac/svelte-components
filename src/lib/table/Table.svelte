779be7aa476e19a49c4ab4fdbcf507a2cd4abe7e
Pouya Saadeghi <pouya.saadeghi@gmail.com>
2022-01-09 18:23:38 +0300

    table: default compact

--- src/lib/table/Table.svelte
+++ src/lib/table/Table.svelte
@@ -6,10 +6,10 @@
 	export let zebra = false
 
 	/** make table more compact */
-	export let compact = false
+	export let compact = true
 </script>
 
-<div class="overflow-x-auto">
+<div class="p-4 overflow-x-auto">
   <table class="table w-full {className}" class:table-zebra={zebra} class:table-compact={compact}>
     <thead>
       <slot name="header" />

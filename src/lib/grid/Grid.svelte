c3b9c592898aec44ee34926ac3404c3ce7f57767
Pouya Saadeghi <pouya.saadeghi@gmail.com>
2022-01-09 18:24:04 +0300

    add grid component

---
+++ src/lib/grid/Grid.svelte
@@ -0,0 +1,16 @@
+<script>
+
+  let className = ''
+	export { className as class }
+
+	/** cols */
+	export let cols = 1
+
+	/** gap */
+	export let gap = 4
+
+</script>
+
+<div class="grid grid-cols-1 md:grid-cols-{cols} gap-{gap} m-{gap} {className}">
+  <slot/>
+</div>

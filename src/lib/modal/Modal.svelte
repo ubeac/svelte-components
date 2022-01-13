8a2f6281caed13f6c565736b18be11607099fdb0
TheHadiAhmadi <TheHadiAhmadi@gmail.com>
2022-01-14 00:27:15 +0430

    update modal component

--- src/lib/modal/Modal.svelte
+++ src/lib/modal/Modal.svelte
@@ -13,18 +13,8 @@
 	export let end = false
 </script>
 
-{#if open}
-	<div
-		transition:fade={{ duration: 100 }}
-		on:click={() => (open = false)}
-		class="fixed w-full h-full z-30 left-0 top-0 bg-black/20 transition-all flex items-center justify-center " />
-	<div
-		class="fixed w-full h-full items-center justify-center top-0 left-0 flex z-50"
-		class:text-center={center}
-		class:text-right={end}>
-		<div
-			class="z-[100] w-10/12 sm:w-8/12 md:w-6/12 lg:w-4/12 h-[90%] overflow-y-scroll bg-base-300 p-4 text-base-content rounded-lg shadow-2xl {className}">
-			<slot />
-		</div>
+<div class="modal" on:click={() => open=false} class:modal-open={open}>
+	<div on:click|stopPropagation class="modal-box {className}" class:text-center={center} class:text-right={end}>
+		<slot/>
 	</div>
-{/if}
+</div>

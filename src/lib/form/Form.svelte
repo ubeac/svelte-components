709d71f5462ce6853d7f85f51ba94bf258d03dcf
TheHadiAhmadi <TheHadiAhmadi@gmail.com>
2022-02-13 19:20:01 +0430

    add Form component

---
+++ src/lib/form/Form.svelte
@@ -0,0 +1,14 @@
+<script>
+  let className = "";
+  export { className as class };
+
+  export let inline = false;
+</script>
+
+<form
+  {...$$restProps}
+  class="w-full flex items-end gap-2 {className}"
+  class:flex-col={!inline}
+>
+  <slot />
+</form>

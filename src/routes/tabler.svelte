59ebe19e9f54ec82e1a1c1bed42432bd3a253e0d
Masood Abdolian <m.abdolian@gmail.com>
2022-02-16 15:36:23 +0330

    init

---
+++ src/routes/tabler.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import { Accordion, AccordionItem } from "sveltestrap";
+</script>
+
+<Accordion>
+  <AccordionItem active header="Home">Fallbrook</AccordionItem>
+  <AccordionItem header="School">
+    <a href="#home">Buena Vista Elementary</a>
+  </AccordionItem>
+  <AccordionItem header="Library">UCSB Library</AccordionItem>
+</Accordion>
+
+<!-- <svelte:head>
+  <link
+    rel="stylesheet"
+    href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css"
+  />
+</svelte:head> -->
+
+<style lang="scss" global>
+  @import "../scss/tabler";
+</style>

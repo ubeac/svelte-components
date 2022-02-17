b7c0d8e8ef76b85ad9ec5127ebf9f2b07bb95ca7
TheHadiAhmadi <TheHadiAhmadi@gmail.com>
2022-02-17 14:55:25 +0430

    use @tabler/core

---
+++ src/routes/tabler.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import { Accordion, AccordionItem } from "sveltestrap";
+</script>
+
+<Accordion>
+  <AccordionItem active header="Home">Fallbrook</AccordionItem>
+  <AccordionItem header="School">
+    <a href="#home">Buena Vista Elementary</a>
+  </AccordionItem>
+  <AccordionItem header="Library">UCSB Library</AccordionItem>
+</Accordion>
+
+<svelte:head>
+  <link
+    rel="stylesheet"
+    href="node_modules/@tabler/core/dist/css/tabler.min.css"
+  />
+</svelte:head>
+<!-- <style lang="scss" global>
+  @import "@tabler/core/dist/css/tabler.min.css";
+</style> -->
